766872c9e5abad54923c063df08fd3b5576092c2
jason.shi <jsoncode@qq.com>
2026-08-11 09:37:08 +0800

    feat: 更新依赖

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -766,6 +766,9 @@ fn extract_ide_icon_from_exe(app: AppHandle, executable: String) -> Result<Strin
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            show_main_window(app);
+        }))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(
